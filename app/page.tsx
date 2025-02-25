d4bdea14279ede6da4147bbb2af7d9c01bd67357
sym5204 <sym5204@163.com>
2025-02-25 18:25:35 +0800

    初始化项目，远程连接测试

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import SplashCursor from "@/Animations/SplashCursor/SplashCursor";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="absolute inset-0 w-screen h-screen -z-10">
+        <SplashCursor />
+      </div>
+
+      
+    </>
+
+  );
+}
